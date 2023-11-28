0df23733c3044be029452793d15f59186c5e5e06
SanzharShadybekov <shadubekovs777@gmail.com>
2023-11-28 13:08:13 +0600

    added tasks

---
+++ logistic/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+
+    def update(self, instance, validated_data):
+
